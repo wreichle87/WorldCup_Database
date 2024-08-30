33c99b236c2f0af622f8e8506c521c085007d68e
wreichle87 <168371665+wreichle87@users.noreply.github.com>
2024-08-30 18:46:00 -0400

    Update insert_data.sh

--- insert_data.sh
+++ insert_data.sh
@@ -10,12 +10,22 @@ fi
 # Do not change code above this line. Use the PSQL variable above to query your database.
 #Truncate all tables for testing purposes
 echo "$($PSQL "TRUNCATE teams,games;")"
-declare -i COUNT=10
 #Read file
-cat games.csv | while IFS="," read YEAR ROUND WINNER OPPENENT WINNER_GOALS OPPONENET_GOALS
+shopt -s lastpipe
+cat games.csv | while IFS="," read YEAR ROUND WINNER OPPONENT WINNER_GOALS OPPONENT_GOALS
 do
-  $COUNT=$(( 1 ))
   #Insert teams into teams table
-  echo $YEAR
+  if [[ $WINNER != "winner" ]]
+  then
+    echo "teams_Insert: $($PSQL "INSERT INTO teams(name) SELECT '$WINNER' WHERE NOT EXISTS (SELECT name FROM teams WHERE name='$WINNER');")"
+    echo "teams_Insert: $($PSQL "INSERT INTO teams(name) SELECT '$OPPONENT' WHERE NOT EXISTS (SELECT name FROM teams WHERE name='$OPPONENT');")"
+    #GET WINNER_ID
+    WINNER_ID="$($PSQL "SELECT team_id FROM teams WHERE name='$WINNER';")"
+    #echo "Winner_id: $WINNER_ID"
+    #GET OPPONENT_ID
+    OPPONENT_ID="$($PSQL "SELECT team_id FROM teams WHERE name='$OPPONENT';")"
+    #echo "Opponent_id: $OPPONENT_ID"
+    #INSERT INTO GAMES TABLE
+    echo "games_Insert: $($PSQL "INSERT INTO games(year, round, winner_id, opponent_id, winner_goals, opponent_goals) VALUES ($YEAR, '$ROUND', $WINNER_ID, $OPPONENT_ID, $WINNER_GOALS, $OPPONENT_GOALS);")" 
+  fi
 done
-echo $COUNT
